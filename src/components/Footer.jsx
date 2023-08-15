6a8fdcdc32dd6630ab4faf1634878fbc4ef69ebf
Jatin Singh Bhandari <102027407+dreamcatstudios@users.noreply.github.com>
2023-08-15 21:09:04 +0530

    main

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,13 @@ const Footer = () => {
     <footer className="bg-blue-700 py-16 mt-32">
       <div className="relative">
         <div className="absolute bg-gray-300 w-[70%] -top-16 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg">
-          <div className="flex flex-row py-5 justify-around">
+          <div className="flex flex-col items-center gap-3 text-center sm:text-left sm:flex-row py-5 justify-around ">
             <div className="flex flex-col">
               <p>Ready to get Started?</p>
               <p>Talk with us today</p>
             </div>
             <div>
-              <button className="bg-purple-500 text-white py-3 px-7 rounded-sm">
+              <button className="bg-purple-500 text-white py-2 px-4 sm:py-3 sm:px-7 rounded-sm">
                 GET STARTED
               </button>
             </div>
